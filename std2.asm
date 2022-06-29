cddaa1b7520295eb7062b3bb11fe36eb8ebe8a63
MarcioShimizu <marcio.shimizu@hotmail.com>
2022-06-29 15:14:44 +0900

    retornando 0 na main

--- std2.asm
+++ std2.asm
@@ -1,10 +1,16 @@
 SECTION .data
 decimal         DB 100   ;valor sem notação
-hexadecimalPOS  DB AFh   ;notação posfixa h
+hexadecimalPOS  DB 0AFh  ;notação posfixa h
 hexadecimalPRE  DB 0xAF  ;notação prefixa 0x
 binarioPOS      DB 0101b ;notação posfixa b
 binarioPRE      DB 0b101 ;notação prefixa 0b
 octalPOS        DB 777o  ;notação posfixa o
 octalPRE        DB 0o777 ;notação prefixa 0o
 ;DD DW DD DQ DT definem a quantidade de bytes destinados para o valor.
-;1,2,4,8,10.
+;1,2,4,8,10.g
+
+SECTION .text 
+GLOBAL main
+main:
+  MOV EAX, 0
+  RET
